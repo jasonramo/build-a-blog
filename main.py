1b2aa3a2f47185cbfe5c19595f3d190e9df1b82f
Jason Ramo <jasonaramo@gmail.com>
2018-10-07 15:09:51 -0500

    set up basic shell for build a blog project

--- main.py
+++ main.py
@@ -1,11 +1,46 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, flash
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:admin@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+app.secret_key = 'mB4kPa934nwmi2o'
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(240))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, name, owner):
+        self.name = name
+        self.completed = False
+        self.owner = owner
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    posts = db.relationship('Blog', backref='owner')
+
+    def __init__(self,email,password):
+        self.email = email
+        self.password = password
 
 @app.route("/")
 def index():
-    return render_template('base.html')
+    if request.method == 'POST':
+            post_name = request.form['post']
+            new_post = Blog(post_name)
+            db.session.add(new_post)
+            db.session.commit()
 
-app.run()
+    posts = Blog.query.all()
+    return render_template('blogs.html',title="Build A Blog", posts=posts)
+
+if __name__ == '__main__':            
+    app.run()
