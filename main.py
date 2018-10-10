12d42dafbf91a72df2ca51f3ee673fe4052adf9e
Jason Ramo <jasonaramo@gmail.com>
2018-10-10 18:41:29 -0500

    added all funtionallity up to the first check point

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:admin@localhost:8889/build-a-blog'
-app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_ECHO'] = True  #provides the sql commands in the terminal
 db = SQLAlchemy(app)
 app.secret_key = 'mB4kPa934nwmi2o'
 
@@ -13,34 +13,36 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(240))
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    #owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, name, owner):
-        self.name = name
-        self.completed = False
-        self.owner = owner
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+        #self.owner = owner
 
-class User(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(120))
-    posts = db.relationship('Blog', backref='owner')
-
-    def __init__(self,email,password):
-        self.email = email
-        self.password = password
-
-@app.route("/")
-def index():
+@app.route('/newpost', methods=['POST', 'GET'])
+def newpost():
     if request.method == 'POST':
-            post_name = request.form['post']
-            new_post = Blog(post_name)
+        post_title_name = request.form['title']
+        post_body_name = request.form['body']
+        if post_title_name == "" or post_body_name == "":
+            flash("You've left a mandatory field blank. Please fill in both fields to post your entry", 'error')
+        else:    
+            new_post = Blog(post_title_name, post_body_name)
+
             db.session.add(new_post)
             db.session.commit()
+            return redirect('/blogs')
+
+    return render_template('newpost.html',title="Build A Blog")
+
+
+@app.route('/blogs', methods=['POST', 'GET'])
+def blogs():
 
     posts = Blog.query.all()
-    return render_template('blogs.html',title="Build A Blog", posts=posts)
+
+    return render_template('blogs.html', title='Build A Blog', posts=posts)
 
 if __name__ == '__main__':            
     app.run()
